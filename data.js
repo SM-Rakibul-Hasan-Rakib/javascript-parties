7b2e1138c94db07025ed436e4527d1707593c4e2
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-09-02 21:18:19 +0600

    3rd commit

---
+++ data.js
@@ -0,0 +1,15 @@
+var Name = "Rakib";
+var Age = "18";
+var Address = "Manda, Dhaka";
+var isMale = true;
+var x = null;
+var y;
+var name = rakib;
+
+console.log(typeof Name); //string;
+console.log(typeof Age); //string;
+console.log(typeof y); //undefined;
+console.log(typeof isMale); //boolean
+console.log(typeof Address); //string
+console.log(typeof x); //object
+// console.log(typeof name); //object
