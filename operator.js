0043c2e1c98613ebf6c22439f1fca28e9c500c2a
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-09-04 21:11:49 +0600

    7th commit

---
+++ operator.js
@@ -0,0 +1,15 @@
+var x = 2;
+var y = 100;
+
+var total = x + y;
+
+console.log(total);
+
+const minus = total - 5000;
+console.log(minus);
+
+var muliple = minus * 4;
+console.log(muliple);
+
+var divide = muliple / 5;
+console.log(divide);
