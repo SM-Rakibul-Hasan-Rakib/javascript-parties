76d28361d3452bf9b071db1565d02ae3ae48a05b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-09-12 01:52:56 +0600

    9th commit

--- if-else.js
+++ if-else.js
@@ -1,6 +1,23 @@
-var abc = 40;
-if (abc === 20) {
-  console.log("abc is a same");
+// var abc = 40;
+// if (abc === 20) {
+//   console.log("abc is a same");
+// } else {
+//   console.log("abc is a not same");
+// }
+// var temputure = 25;
+
+// if (temputure > 30) {
+//   console.log("It's a hot day");
+// } else if (temputure > 20) {
+//   console.log("It's a warm day");
+// } else if (temputure > 10) {
+//   console.log("It's a cold day");
+// }
+
+var x = 7;
+
+if (x > 5 || x > 10) {
+  console.log("true");
 } else {
-  console.log("abc is a not same");
+  console.log("false");
 }
