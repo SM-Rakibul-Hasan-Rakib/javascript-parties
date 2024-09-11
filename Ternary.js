9d62349e320a048cb0efa7f1894f9bb45d3b942d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-09-12 02:03:50 +0600

    10th commit

---
+++ Ternary.js
@@ -0,0 +1,13 @@
+var y = 10;
+var message;
+// if (y > 5) {
+//   message = "biggest than 5";
+// } else {
+//   message = "less than 5";
+// }
+// console.log(message);
+
+// Rules: condition ? true : false;
+// message = y < 5 ? "biggest than 5" : "less than 5";
+// console.log(message);
+y > 5 ? console.log("bigger than 5") : console.log("less than 5");
