59af7d02da3699b6251efb9a4c2893530abd6cf2
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-09-24 22:30:23 +0600

    for loop file add

---
+++ loop.js
@@ -0,0 +1,5 @@
+let mal = 1;
+while (mal <= 1000) {
+  console.log(mal + " . Lab nai Fatema tora valobashe na ");
+  mal++;
+}
---
+++ while.js
@@ -0,0 +1,21 @@
+let i = 1;
+
+while (i <= 100) {
+  console.log(i + " Sorry jaan");
+  i++;
+}
+
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//   sum += i;
+//   i++;
+// }
+
+// console.log(sum);
+let maisha = 1;
+
+while (maisha <= 100) {
+  console.log(maisha + " Porta bhoso smna DU lakbo ");
+  maisha++;
+}
