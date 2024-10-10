246d2e8063c4af42c29bfd8422e17a9f20ac4d9b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-10-10 17:23:35 +0600

    added

--- for.js
+++ for.js
@@ -1,3 +1,29 @@
-for (let i = 1; i <= 50; i += 1) {
-  console.log(i, "Zabair senior ar sata pprem kore");
+// const nums = [20, 30, 40, 50, 506, 89];
+
+// let sumOfOdd = 0;
+// let sumOfEven = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//   const x = nums[i];
+
+//   if (x % 2 === 1) {
+//     sumOfOdd += x;
+//   } else {
+//     sumOfEven += x;
+//   }
+// }
+
+// carNAme = "volvo";
+// var carNAme;
+// console.log(carNAme);
+
+// let x = 5;
+// x += 5;
+
+// console.log(x);
+
+function sleep() {
+  console.log("Rakib is sleeping from 10 pm");
 }
+sleep();
+console.log("my name");
